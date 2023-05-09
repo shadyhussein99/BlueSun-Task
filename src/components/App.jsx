46ee50a20e0aaa3ed602531bee6c6bcd4838a3af
Shady Hussein <shadyhussein99@gmail.com>
2023-05-09 21:36:15 +0200

    Setting the logic of showing the chart of the selected customer only

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import Table from "./Table"
+import CustomersDropdownMenu from "./CustomersDropdownMenu"
 import BarChart from "./BarChart"
 
 function App() {
 
+  const [customersChart, setCustomersChart] = useState({
+    customer1: false,
+    customer2: false,
+    customer3: false,
+    customer4: false,
+    customer5: false,
+  })
 
   return (
     <>
       <Table />
+      <CustomersDropdownMenu 
+        customersChart={customersChart}
+        setCustomersChart={setCustomersChart}
+      />
       <BarChart />
     </>
   )
--- src/components/CustomersDropdownMenu.jsx
+++ src/components/CustomersDropdownMenu.jsx
@@ -1,8 +1,62 @@
+// The component of the dropdown menu
 
+import { useState } from "react"
+import { useSelector } from "react-redux"
 
-function CustomersDropdownMenu() {
+function CustomersDropdownMenu(props) {
+
+    const data = useSelector((state) => state.data.value)    // Data of the json file
+    const [toggle, setToggle] = useState(false)
+
+    // The logic used to show the chart of the selected customer only from the dropdown menu
+    const handleClick = (index) => {
+        if (index === 0) {
+            const updatedObject = Object.keys(props.customersChart).reduce((acc, curr) => {
+                acc[curr] = false;
+                return acc;
+            }, {});
+            props.setCustomersChart({ ...updatedObject, customer1: true });
+
+        } else if (index === 1) {
+            const updatedObject = Object.keys(props.customersChart).reduce((acc, curr) => {
+                acc[curr] = false;
+                return acc;
+            }, {});
+            props.setCustomersChart({ ...updatedObject, customer2: true });
+
+        } else if (index === 2) {
+            const updatedObject = Object.keys(props.customersChart).reduce((acc, curr) => {
+                acc[curr] = false;
+                return acc;
+            }, {});
+            props.setCustomersChart({ ...updatedObject, customer3: true });
+
+        } else if (index === 3) {
+            const updatedObject = Object.keys(props.customersChart).reduce((acc, curr) => {
+                acc[curr] = false;
+                return acc;
+            }, {});
+            props.setCustomersChart({ ...updatedObject, customer4: true });
+
+        } else if (index === 4) {
+            const updatedObject = Object.keys(props.customersChart).reduce((acc, curr) => {
+                acc[curr] = false;
+                return acc;
+            }, {});
+            props.setCustomersChart({ ...updatedObject, customer5: true });
+        }
+    }
+
     return <div>
-        <h3>Show Customers Charts</h3>
+        <button onClick={() => { setToggle(!toggle) }}>Show Customers Charts 🔽</button>
+
+        {toggle && (
+            <div>
+                {data.customers.map((value, index) => {
+                    return <button key={value.id} onClick={() => handleClick(index)}>{value.name}</button>
+                })}
+            </div>
+        )}
     </div>
 }
 
