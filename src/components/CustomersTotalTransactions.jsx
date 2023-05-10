2c9bf9165f075d3e978ee095d5c813ad9bf54d2a
Shady Hussein <shadyhussein99@gmail.com>
2023-05-10 20:18:47 +0200

    styling large screens

--- src/components/CustomersTotalTransactions.jsx
+++ src/components/CustomersTotalTransactions.jsx
@@ -8,11 +8,11 @@ function CustomersTotalTransactions(props) {
 
     return <section className="text-center text-lg font-semibold mb-20 lg:text-xl xl:text-2xl">  {/* Used or (||) operator to return the amount directly if the customer made only one transaction as the reduce method returns an object if the array has only one element */}
 
-        {props.customer1Transaction && <p>{data.customers[0].name} total transactions is {props.customer1Data.amount || props.customer1Data}</p>}
-        {props.customer2Transaction && <p>{data.customers[1].name} total transactions is {props.customer2Data.amount || props.customer2Data}</p>}
-        {props.customer3Transaction && <p>{data.customers[2].name} total transactions is {props.customer3Data.amount || props.customer3Data}</p>}
-        {props.customer4Transaction && <p>{data.customers[3].name} total transactions is {props.customer4Data.amount || props.customer4Data}</p>}
-        {props.customer5Transaction && <p>{data.customers[4].name} total transactions is {props.customer5Data.amount || props.customer5Data}</p>}
+        {props.customer1Transaction && <p>{data.customers[0].name}'s total transactions is {props.customer1Data.amount || props.customer1Data}</p>}
+        {props.customer2Transaction && <p>{data.customers[1].name}'s total transactions is {props.customer2Data.amount || props.customer2Data}</p>}
+        {props.customer3Transaction && <p>{data.customers[2].name}'s total transactions is {props.customer3Data.amount || props.customer3Data}</p>}
+        {props.customer4Transaction && <p>{data.customers[3].name}'s total transactions is {props.customer4Data.amount || props.customer4Data}</p>}
+        {props.customer5Transaction && <p>{data.customers[4].name}'s total transactions is {props.customer5Data.amount || props.customer5Data}</p>}
 
     </section>
 }
