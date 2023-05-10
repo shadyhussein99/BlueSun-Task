3acccfedec7c732dc34fd94bade2f41b2f175b1f
Shady Hussein <shadyhussein99@gmail.com>
2023-05-10 20:14:37 +0200

    styling large screens

--- src/components/HighestTransactionCustomer.jsx
+++ src/components/HighestTransactionCustomer.jsx
@@ -2,7 +2,7 @@
 
 function HighestTransactionCustomer(props) {
     return <section className="my-7 text-center lg:text-xl lg:my-12">
-        <p>⭐<span className="text-xl font-semibold">{props.customerHighestTransaction.name}</span> has the highest transaction record with {props.customerHighestTransaction.amount}</p>
+        <p>⭐<span className="text-xl font-semibold">{props.customerHighestTransaction.name}</span> has the highest transaction record with {props.customerHighestTransaction.amount} total transactions</p>
     </section>
 }
 
